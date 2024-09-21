7dc2d7050b095853ac91db98d9d19f0d144c32cb
rrojasPy <60195271+rrojasPy@users.noreply.github.com>
2024-09-21 15:36:14 -0400

    Update autoSend.py

--- autoSend.py
+++ autoSend.py
@@ -1,31 +1,49 @@
-#pip install pandas
-#pip install pycopy-webbrowser
+import os
 import pandas as pd
 import webbrowser as web
 import time
+import pyautogui
 
+def procesar_mensajes(path):
+    if not os.path.isfile(path):
+        print(f"El archivo no existe: {path}")
+        return
 
+    df = pd.read_csv(path)
+    contadorSI = 0
+    contadorNO = 0
 
+    for fila in df.index:
+        try:
+            if df.at[fila, "enviar"] == "SI":
+                contadorSI += 1
+                web.open(df.at[fila, "enlace"])
+                time.sleep(12)  # Espera de 10 segundos entre cada apertura de enlace
 
-path="C:/Users/Rodney/PycharmProjects/TecnoTest/_DataMoodle_Febrero2023.csv"
-df=pd.read_csv(path)
-contadorSI =0
-contadorNO =0
+                pyautogui.press('enter')  # Simula la presión de la tecla Enter
+                pyautogui.write('\n')
+                time.sleep(5)  # Espera adicional de 6 segundos
 
-for fila in df.index:
+                pyautogui.press('enter')  # Simula la presión de la tecla Enter
+                time.sleep(2)  # Espera de 1 segundo para asegurar que la pestaña se cierre
 
-    if df["Enviar"][fila]=="SI":
-        contadorSI+=1
-        web.open(df["enlace"][fila])
-        time.sleep(10)
+                pyautogui.hotkey('ctrl', 'w')
+                time.sleep(1)  # Espera de 1 segundo para asegurar que la pestaña se cierre
 
-    else:
-        contadorNO +=1
-        print("No enviado.")
+                pyautogui.press('enter')  # Simula la presión de la tecla Enter
+                time.sleep(2)  # Espera de 1 segundo para asegurar que la pestaña se cierre
 
-    print(f"ultimo envio -> {contadorSI} ")
-    # time.sleep(9000)
+            else:
+                contadorNO += 1
+                print("No enviado.")
+            print(f"Último envío -> {contadorSI} / {df.count()[0]}")
+            
+        except Exception as e:
+            print(f"Error al procesar la fila {fila}: {e}")
 
-    # web.get(chrome_path).open("https://web.whatsapp.com/send?phone=" + celular + "&text=" + mensaje)
-print(f"Se envio {contadorSI} de {str(df.axes[0])}")
-print(f"No se envio {contadorNO} de {str(df.axes[0])}")
+    print(f"Se enviaron {contadorSI} de {len(df)} mensajes.")
+    print(f"No se enviaron {contadorNO} de {len(df)} mensajes.")
+
+# Reemplaza el siguiente camino con la ruta correcta de tu archivo CSV
+path = r"Whatsapp_moodle_messages.csv"
+procesar_mensajes(path)
